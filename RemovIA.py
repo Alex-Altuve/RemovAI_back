4ba81a9fafd5a6edb7e5494eb3b984dd08809736
Alex-Altuve <altuvealex6@gmail.com>
2024-05-12 18:54:29 -0400

    local api with endpoint -eliminar_fondo-

---
+++ RemovIA.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+import Replicate_api as modelo
+
+app = Flask(__name__)
+
+@app.route('/eliminar_fondo', methods=['POST'])
+def remove_backgroundIA():
+    try:
+        data = request.json
+        url = data.get('url')
+        if not url:
+            return jsonify({'error': 'URL missing in request body'}), 400
+
+        output_url = modelo.remove_background(url)
+        return jsonify({'output_url': output_url})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
