3de7430668d5fce8fdcbdd840f681b20d1dd5ef8
AlejandroNovellino <alnovellino07@gmail.com>
2024-05-20 13:17:10 -0400

    add endpoint for delete background from file

--- RemovIA.py
+++ RemovIA.py
@@ -13,7 +13,7 @@ CORS(app)
 SWAGGER_URL = "/swagger"
 API_URL = "/static/swagger.json"
 # flask files constants
-UPLOAD_FOLDER = "/static/uploads/"
+UPLOAD_FOLDER = "./static/uploads/"
 ALLOWED_EXTENSIONS = {"mp4"}
 
 app = Flask(__name__)
@@ -34,63 +34,62 @@ def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route("/eliminar-fondo", methods=["POST"])
+@app.route("/delete-background/url", methods=["POST"])
 @cross_origin()
-def remove_background_ai():
+def remove_background_url():
     """
     function to remove the background of a video from URL
     """
     try:
+        print("Hi form delete background from URL")
+        print("Request data", request.data)
+        # if no file exists then verify the URL
         data = request.json
-        print(data)
-        url = data.get("input")
-        if not url:
+        print("Data in request", data)
+        video = data.get("input")
+        if not video:
+            print("No url data in request", data)
             return jsonify({"error": "URL missing in request body"}), 400
-
-        output_url = modelo.remove_background(url)
-        return jsonify({"output_url": output_url})
+        # call the model
+        output = modelo.remove_background(video=video)
+        # return the result
+        return jsonify({"output_url": output})
     except Exception as e:
         return jsonify({"error": str(e)})
 
 
-@app.route("/delete-background", methods=["POST"])
+@app.route("/delete-background/file", methods=["POST"])
 @cross_origin()
-def remove_background_url_or_video_ai():
+def remove_background_file():
     """
-    function to remove the background of a video from URL
+    function to remove the background of a video from a file
     """
     try:
+        print("Hi form delete background from file")
         print("Request data", request.data)
         print("Request files", request.files)
-        # variable for the result
-        video = {}
         # verify if the user submitted a file or a URL
         if "video" in request.files:
             print(request.files)
-            file = request.files["file"]
+            file = request.files["video"]
             # if the user uploaded a empty file without a name
             if file.filename == "":
                 print("No file selected")
                 return jsonify({"error": "No file selected"}), 400
             # if the file is not empty and is allowed
             if file and allowed_file(file.filename):
+                # create a secure file object
                 filename = secure_filename(file.filename)
                 # save the file to local
                 file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
                 # open the video
-                video = open("./static/uploads/video.mp4", "rb")
-        else:
-            # if no file exists then verify the URL
-            data = request.json
-            print("Data in request", data)
-            video = data.get("input")
-            if not video:
-                print("No url data in request", data)
-                return jsonify({"error": "URL missing in request body"}), 400
-        # call the model
-        output = modelo.remove_background(video=video)
-        # return the result
-        return jsonify({"output_url": output})
+                video = open(f"./static/uploads/{filename}", "rb")
+                # call the model
+                output = modelo.remove_background(video=video)
+                # return the result
+                return jsonify({"output_url": output})
+            else:
+                return jsonify({"error": "Invalid video extension"}), 400
     except Exception as e:
         return jsonify({"error": str(e)})
 
